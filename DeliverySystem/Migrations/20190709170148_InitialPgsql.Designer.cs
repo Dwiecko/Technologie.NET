450dff48b38e8041b05f54f67080cdd79b8249d5
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-07-09 19:44:50 +0200

    Heroku Deployment

--- DeliverySystem/Migrations/20190709170148_InitialPgsql.Designer.cs
+++ DeliverySystem/Migrations/20190709170148_InitialPgsql.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using DeliverySystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace DeliverySystem1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190709170148_InitialPgsql")]
+    partial class InitialPgsql
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
